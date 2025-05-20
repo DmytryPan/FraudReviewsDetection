ccc08eaeef1c9208cefd5cde52ace3f6c7a3fb13
DmytryPan <panikhidin@sfedu.ru>
2025-05-20 17:59:11 +0300

    add clasify logic

--- review_classifier/classifier/views.py
+++ review_classifier/classifier/views.py
@@ -1,46 +1,120 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
+import os
+import numpy as np
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
 from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-import joblib
+from nltk.tokenize import word_tokenize
+import pickle
 import os
 import re
-# Create your views here.
+import torch
+from torch import nn
+import contractions 
+import re
 
-model = joblib.load('classifier/models/RandomForestClassifier.joblib')
-vectorizer = joblib.load('classifier/models/rf_vectorizer.joblib')
-stop_words = set(stopwords.words('english'))
+# класс модели-классификатора
+class lstm_parallel_cnn(nn.Module):
+   def __init__(self, vocab_size, embedding_matrix, embedding_dim, hidden_dim, num_filters, kernels_sizes, output_dim, dropout_rate, num_layers):
+       super(lstm_parallel_cnn, self).__init__()
+       embedding_matrix = torch.tensor(embedding_matrix, dtype=torch.float32)
+       self.embedding = nn.Embedding.from_pretrained(embedding_matrix)
+       self.dropout = nn.Dropout(dropout_rate)
+       self.lstm = nn.LSTM(input_size=embedding_dim, 
+                           hidden_size=hidden_dim, 
+                           batch_first=True,
+                           bidirectional=True,
+                           num_layers=num_layers, 
+                           dropout=dropout_rate if num_layers > 1 else 0,
+                           )
+       # Список сверточных слоев
+       self.conv_lst = nn.ModuleList([
+           nn.Sequential(
+               nn.Conv1d(in_channels=hidden_dim*2,out_channels=num_filters,kernel_size=ks),
+               nn.BatchNorm1d(num_filters),
+               nn.ReLU(),
+               nn.Dropout(dropout_rate),
+               nn.AdaptiveMaxPool1d(1),
+               nn.Flatten()
+                        ) for ks in kernels_sizes])
+       
+       self.bn = nn.BatchNorm1d(num_filters)
+       self.relu = torch.nn.ReLU()
+       
+       self.fc1 = nn.Linear(num_filters*len(kernels_sizes), output_dim) 
+   def forward(self, x):
+       embedded = self.dropout(self.embedding(x))
+       lstm_out,_ = self.lstm(embedded)
+       lstm_out = lstm_out.permute(0, 2, 1)
+       conv_outs = [conv(lstm_out) for conv in self.conv_lst]
+       conv_out_cat = torch.cat(conv_outs, dim=1)
+       out = self.fc1(conv_out_cat)
+       return out
 
-stemmer = SnowballStemmer('english')
+# функция предобработки текста отзыва
+def preprocess_review(text):
+    text = contractions.fix(text)
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z\d'\s]", '', text)
+    words = word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
+    return ' '.join(words)
 
-def preprocess(text):
-    text = re.sub(r'<.*?>', '', text)
-    emoticons = re.findall(r'(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-    text = (re.sub(r'[\W]+', ' ', text)) + ' ' + ' '.join(emoticons).replace('-', '')
-    tokens = [word for word in text.split() if word.lower() not in stop_words]
-    tokens = [stemmer.stem(token) for token in tokens]
-    text = ' '.join(tokens)
-    return text
+# функция векторизации текста
+def text_to_tensor(text, vocabulary, max_len=256):
+    cl_text = preprocess_review(text)
+    tokens = cl_text.split()[:max_len] # берем токены
+    inds = [vocabulary.get(tok, vocabulary['UKN']) for tok in tokens] # вычисляем их индексы
+    padding_length = max_len - len(inds)
+    inds += [vocabulary['PAD']] * padding_length
+    return torch.tensor(inds).unsqueeze(0)
+
+def predict(model, text):
+    model.eval()
+    with torch.no_grad():
+        input = text_to_tensor(text, vocabulary)
+        output = model(input)
+        prob = torch.sigmoid(output).item()
+        label = 1 if prob >= 0.5 else 0
+        return label
+
+stop_words = set(stopwords.words('english')) # стоп-слова
+
+# print(os.getcwd())
+vocabulary = {}
+with open('classifier/embeddings/vocabulary.pkl', 'rb') as f:
+    vocabulary =  pickle.load(f) # отображение слово - индекс в матрице
+
+# print(len(vocabulary))
+
+embeddings_matrix = np.load('classifier/embeddings/embedding_matrix.npy') # матрица вложений слов
+
+# Параметры модели
+embedding_dim = 300
+hidden_dim = 256
+num_layers = 1
+kernels_sizes = [2,3,4]
+num_filters = 256
+output_dim = 1
+
+# создание и загрузка модели
+model = lstm_parallel_cnn(len(vocabulary), embeddings_matrix, embedding_dim, hidden_dim, num_filters, kernels_sizes, output_dim, 0.25, 1)
+model.load_state_dict(torch.load('classifier/weights/lstm_parallel_cnn.pth',  weights_only=True, map_location=torch.device('cpu')))
+print('модель загружена')
+
+# predict(model, "test text example")
 
 def classify_review(request):
-    print(os.getcwd())
     if request.method =='POST':
-        review = preprocess(request.POST.get('review')) # Извлекли текст ревью из POST запроса и предобработали
-        transformed_review = vectorizer.transform([review]) # Векторизация переданного текста 
-        pred = model.predict(transformed_review)
-        probs = model.predict_proba(transformed_review)
-
-        negative_prob, positive_prob = probs[0][0], probs[0][1]
-
-        rating =  5 + 5*positive_prob if pred[0] == 1 else 5 * (1 - negative_prob)
-        
+        review = request.POST.get('review')
+        if not review:
+            return JsonResponse({'error': 'Текст не передан'}, status=400)
+        label = predict(model, review)
         result = {
-            'prediction': 'positive' if pred[0] == 1 else 'negative',
-            'rating' : round(rating, 1) 
+            'prediction': 'Реальный отзыв' if label == 1 else 'Фейковый отзыв'
         }
         return JsonResponse(result)
     return render(request, 'classifier/classify_review.html')
